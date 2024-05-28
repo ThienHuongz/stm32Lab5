659ce552b9e6c4742e2ba28fc963b7f6dcc1e6ea
ThienHuongZz <huongy138@gmail.com>
2024-05-28 15:28:35 +0700

    lab 5

---
+++ Lab5_NTThienHuong/Core/Src/fsm_uart.c
@@ -0,0 +1,99 @@
+/*
+ * fsm_uart.c
+ *
+ *  Created on: May 22, 2024
+ *      Author: glmun
+ */
+#include "fsm_uart.h"
+
+
+
+uint8_t temp = 0;
+
+uint8_t buffer [ MAX_BUFFER_SIZE ] = {'\0'};
+uint8_t index_buffer = 0;
+
+uint8_t buffer_flag = 0;
+
+uint32_t ADC_value = 0;
+char str[20];
+
+int commandStatus = 1;
+int uartStatus = 1;
+
+
+void HAL_UART_RxCpltCallback ( UART_HandleTypeDef * huart ) {
+	if( huart -> Instance == USART2 ) {
+		buffer [ index_buffer ++] = temp ;
+		if( index_buffer == 30) index_buffer = 0;
+		buffer_flag = 1;
+		HAL_UART_Transmit (& huart2 , & temp , 1 , 50) ;
+		HAL_UART_Receive_IT (&huart2 , & temp , 1) ;
+	}
+}
+
+void command_parser_fsm() {
+    switch (commandStatus) {
+    	// first char of command
+        case 1:
+            if (temp == '!') {
+            	commandStatus = 2;
+            }
+            break;
+        // end of command
+        case 2:
+            if (temp == '#') {
+            	commandStatus = 1;
+                uartStatus = 2;
+            }
+            break;
+        default:
+            break;
+    }
+}
+void clear(){
+	for (int i = 0; i < MAX_BUFFER_SIZE; i++) {
+	        buffer[i] = '\0';
+	    }
+	    index_buffer = 0;
+}
+
+char* getBuffer() {
+    return (char*) buffer;
+}
+void uart_communication_fsm() {
+    switch (uartStatus) {
+        case 1:
+            break;
+        case 2:
+        	// string comapre
+
+//        	HAL_UART_Transmit(&huart2, (void *)getBuffer(), strlen(getBuffer()), 1000);
+
+            if (strncmp(getBuffer(), "!RST#",5) == 0) {
+                ADC_value = HAL_ADC_GetValue(&hadc1);
+                HAL_UART_Transmit(&huart2, (void *)str,  sprintf(str, "\r\n!ADC=%ld#", ADC_value), 1000);
+                setTimer1(300);
+                clear();
+                uartStatus = 3;
+            } else {
+                uartStatus = 1;
+            }
+            clear();
+            break;
+        case 3:
+
+            if (timer1_flag == 1) {
+
+                ADC_value = HAL_ADC_GetValue(&hadc1);
+                HAL_UART_Transmit(&huart2, (void*) str,  sprintf(str, "\r\n!ADC=%ld#", ADC_value), 1000);
+                setTimer1(300);
+            }
+            if (strncmp(getBuffer(), "!OK#",4) == 0) {
+                uartStatus = 1;
+            }
+            break;
+        default:
+            break;
+    }
+}
